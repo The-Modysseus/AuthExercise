4b074413a616056e33a5f024ad6c3f11a5a5bc9f
Mohamad Farhoud <Mohamadfarhoud@hotmail.com>
2023-11-12 18:33:19 +0100

    Added authentication service and middleware. Updatted appsettings.json

--- Program.cs
+++ Program.cs
@@ -29,7 +29,27 @@ builder.Services.AddIdentity<ApiUser, IdentityRole>(options =>
 	options.Password.RequireNonAlphanumeric = true;
 	options.Password.RequiredLength = 8;
 }).AddEntityFrameworkStores<ApplicationDbContext>();
-
+builder.Services.AddAuthentication(options =>
+{
+	options.DefaultAuthenticateScheme =
+	options.DefaultChallengeScheme =
+	options.DefaultForbidScheme =
+	options.DefaultScheme =
+	options.DefaultSignInScheme =
+	options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+	options.TokenValidationParameters = new TokenValidationParameters
+	{
+		ValidateIssuer = true,
+		ValidIssuer = builder.Configuration["JWT:Issuer"],
+		ValidateAudience = true,
+		ValidAudience = builder.Configuration["JWT:Audience"],
+		ValidateIssuerSigningKey = true,
+		IssuerSigningKey = new SymmetricSecurityKey(
+			System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"]))
+	};
+});
 
 var app = builder.Build();
 
@@ -41,6 +61,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
