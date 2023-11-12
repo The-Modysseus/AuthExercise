c25614bb13f8816e48508df53c5854c9642d2183
Mohamad Farhoud <Mohamadfarhoud@hotmail.com>
2023-11-12 19:06:21 +0100

    Begin implementation of AccountController and added RegisterDTO

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
-
+using AuthExercise.Models;
+using AuthExercise.DTO;
 
 namespace AuthExercise.Controllers
 {
@@ -9,7 +10,44 @@ namespace AuthExercise.Controllers
 	public class AccountController : ControllerBase
 	{
 		private readonly ApplicationDbContext _context;
-		private readonly ILogger<>
+		private readonly ILogger<DrinkController> _logger;
+		private readonly IConfiguration _configuration;
+		private readonly UserManager<ApiUser> _userManager;
+		private readonly SignInManager<ApiUser> _signInManager;
+
+		public AccountController(ApplicationDbContext context, ILogger<DrinkController> logger, IConfiguration configuration, UserManager<ApiUser> userManager, SignInManager<ApiUser> signInManager)
+		{
+			_context = context;
+			_logger = logger;
+			_configuration = configuration;
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+
+		public async Task<ActionResult> Register(RegisterDTO input)
+		{
+			try
+			{
+                if (ModelState.IsValid)
+                {
+					var newUser = new ApiUser();
+					newUser.UserName = input.Email;
+					newUser.Email = input.Email;
+					newUser.FullName = input.FullName;
+					var result = await _userManager.CreateAsync(newUser, input.Password);
+					if (result.Succeeded)
+					{
+						_logger.LogInformation("User {userName} ({email}) has been created.", newUser.UserName, newUser.Email);
+						return StatusCode(201, $"User '{newUser.UserName}' has been created.");
+					}
+					else
+					{
+						throw new Exception(string.Format("Error: {0}", string.Join(" ", result.Errors.Select(e => e.Description))));
+					}
+                }
+				else
+            }
+		}
 	}
 
 }
